f2da669a30c8a154d226a75665e13954ed9eaf3a
Wat Hughes <wat.hughes@gmail.com>
2015-12-20 18:30:09 -0500

    WIP

--- EDA.R
+++ EDA.R
@@ -1,9 +1,56 @@
 # TBD
 
 library(tm)
+install.packages('ngram')
+library(ngram)
 
 DataDir = '../data'
 EnUsDir = paste0(DataDir,'/en_US')
+
+# Remove all documents that contain any of these words. The object is to avoid
+# training with phrases that require profanity to convey a thought. That helps
+# the final application of the model avoid 'going there'. Note that the user can
+# still enter profanity in which case the app will have to fall back on its
+# unknown word processing. Also note this list is somewhat arbitrary (I like it)
+# and that this use of ProfaneWords is very much a simplification. For example,
+# 'pussy cat' is not profane. But most commonly, I expect 'pussy' to be very
+# profane so its here.
+
+ProfaneWords = c( # Similar to https://en.wikipedia.org/wiki/Seven_dirty_words
+     'asshole', 'cocksucker', 'cunt', 'fuck', 'motherfucker', 'piss',
+     'pussy', 'shit', 'tits'
+)
+
+# LoadTextFile uses readLines on the indicated file assuming that it is UTF-8
+# and put the resulting vector in the global environment as ObjectName.
+# For convenience, return it. Optionaly report the load time.
+
+LoadTextFile = function(FileName,DataDir='.',ObjectName=FileName,
+                        MaxRowsToRead=-1,verbose=F)
+{
+    StartTime = proc.time()
+    FN = paste0(DataDir,'/',FileName)
+    con = file(FN,open='rb') # So that Control-Z doesn't stop readLines since that is valid UTF-8
+    assign(ObjectName,
+           readLines(con,n=MaxRowsToRead,encoding='UTF-8'),
+           envir=.GlobalEnv)
+    close(con)
+    LoadTime = proc.time()
+    LoadTime = LoadTime - StartTime
+    ret = get(ObjectName)
+    if (verbose == T)
+    {
+        nrows = length(ret)
+        print(paste0(format(nrows,big.mark=','),' rows loaded in:'))
+        print(LoadTime)
+    }
+    ret
+} # LoadTextFile
+
+invisible(LoadTextFile('en_US.blogsU.txt',EnUsDir,'Blogs',verbose=T))
+invisible(LoadTextFile('en_US.newsU.txt',EnUsDir,'News',verbose=T))
+invisible(LoadTextFile('en_US.twitterU.txt',EnUsDir,'Twitter',verbose=T))
+
 # fb = paste0(EnUsDir,'/en_US.blogs.txt')
 # fn = paste0(EnUsDir,'/en_US.news.txt')
 # ft = paste0(EnUsDir,'/en_US.twitter.txt')
@@ -41,4 +88,14 @@ length(ln)
 # [1] 77259 # Hmmm, should be 1,010,242
 head(Encoding(lb)) # [1] "UTF-8"   "unknown" "unknown" "unknown" "unknown" "unknown"
 head(lb)
+
+table(Encoding(Blogs))
+table(Encoding(News))
+table(Encoding(Twitter))
+# unknown   UTF-8
+#  636261  263027
+# unknown   UTF-8
+#  874277  135965
+# unknown   UTF-8
+# 2282716   77432
 
