55b48785d7a9346d72ee00d0eb50386aee895d2e
Wat Hughes <wat.hughes@gmail.com>
2015-12-21 16:01:25 -0500

    Initial plots

--- EDA.R
+++ EDA.R
@@ -1,8 +1,11 @@
 # TBD
 
-library(tm)
-install.packages('ngram')
-library(ngram)
+# library(tm) # Not really helpful for this project
+# install.packages('ngram') # Does not appear to be useful
+# library(ngram)
+# install.packages('quanteda')
+library(quanteda)
+require(RColorBrewer)
 
 DataDir = '../data'
 EnUsDir = paste0(DataDir,'/en_US')
@@ -21,35 +24,53 @@ ProfaneWords = c( # Similar to https://en.wikipedia.org/wiki/Seven_dirty_words
      'pussy', 'shit', 'tits'
 )
 
+# Thoughts on punctuation. There's information content in the sentence ending
+# period, question mark, and exclamation point. By that, I mean that n-grams
+# shouldn't cross that boundary. Apostrophe is part of a word. <sigh> Other uses
+# of punctuation include URLs, hastags, etc. and I think are best left alone as
+# part of words because the resulting ngrams will be rare so won't influence the
+# results.
+
+# 'Words' like RT probably wont' be entered into the app so I wonder if I should
+# remove tweets that contain them?
+
+
+
 # LoadTextFile uses readLines on the indicated file assuming that it is UTF-8
 # and put the resulting vector in the global environment as ObjectName.
-# For convenience, return it. Optionaly report the load time.
+# For convenience, return it. Optionaly report the load time, return a sample,
+# and/or limit the number of rows read.
 
 LoadTextFile = function(FileName,DataDir='.',ObjectName=FileName,
-                        MaxRowsToRead=-1,verbose=F)
+                        SampleSize=-1,MaxRowsToRead=-1,verbose=F)
 {
     StartTime = proc.time()
     FN = paste0(DataDir,'/',FileName)
     con = file(FN,open='rb') # So that Control-Z doesn't stop readLines since that is valid UTF-8
-    assign(ObjectName,
-           readLines(con,n=MaxRowsToRead,encoding='UTF-8'),
-           envir=.GlobalEnv)
+    ret = readLines(con,n=MaxRowsToRead,encoding='UTF-8');
     close(con)
     LoadTime = proc.time()
     LoadTime = LoadTime - StartTime
-    ret = get(ObjectName)
     if (verbose == T)
     {
         nrows = length(ret)
         print(paste0(format(nrows,big.mark=','),' rows loaded in:'))
         print(LoadTime)
     }
+    if (SampleSize > 0)
+    {
+        ret = sample(ret, SampleSize)
+    }
+    assign(ObjectName,
+           ret,
+           envir=.GlobalEnv)
     ret
 } # LoadTextFile
 
-invisible(LoadTextFile('en_US.blogsU.txt',EnUsDir,'Blogs',verbose=T))
-invisible(LoadTextFile('en_US.newsU.txt',EnUsDir,'News',verbose=T))
-invisible(LoadTextFile('en_US.twitterU.txt',EnUsDir,'Twitter',verbose=T))
+SampleSize = 100000
+invisible(LoadTextFile('en_US.blogsU.txt',EnUsDir,'Blogs',SampleSize,verbose=T))
+invisible(LoadTextFile('en_US.newsU.txt',EnUsDir,'News',SampleSize,verbose=T))
+invisible(LoadTextFile('en_US.twitterU.txt',EnUsDir,'Twitter',SampleSize,verbose=T))
 
 # fb = paste0(EnUsDir,'/en_US.blogs.txt')
 # fn = paste0(EnUsDir,'/en_US.news.txt')
@@ -98,4 +119,35 @@ table(Encoding(Twitter))
 #  874277  135965
 # unknown   UTF-8
 # 2282716   77432
+
+
+BlogsC=corpus(Blogs)
+# BlogsT = tokenize(BlogsC)
+tokenInfoB=summary(BlogsC)
+BlogsD = dfm(BlogsC)
+NewsC=corpus(News)
+# NewsT = tokenize(NewsC)
+tokenInfoC=summary(NewsC)
+NewsD = dfm(NewsC)
+TwitterC=corpus(Twitter)
+# TwitterT = tokenize(TwitterC)
+tokenInfoT=summary(TwitterC)
+TwitterD = dfm(TwitterC)
+
+par(mfrow=c(1,3))
+hist(tokenInfoB$Tokens,xlab='Word Count',main='Blogs')
+hist(tokenInfoC$Tokens,xlab='Word Count',main='News')
+hist(tokenInfoT$Tokens,xlab='Word Count',main='Tweets')
+
+layout(matrix(c(1,4,2,5,3,6),nrow=2),heights=c(lcm(1), lcm(6)))
+par(mar=rep(0, 0))
+plot.new()
+text(x=0.5,y=0.5,cex=2,'Blogs')
+frame()
+text(x=0.5,y=0.5,cex=2,'News')
+frame()
+text(x=0.5,y=0.5,cex=2,'Tweets')
+plot(BlogsD, max.words=100,colors=brewer.pal(6,'Dark2'),scale=c(8,.5))
+plot(NewsD, max.words=100,colors=brewer.pal(6,'Dark2'),scale=c(8,.5))
+plot(TwitterD, max.words=100,colors=brewer.pal(6,'Dark2'),scale=c(8,.5))
 
